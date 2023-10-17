0f0ef038eae4636f0235a0138ef889dc81056a4c
George Albadr <georgealbadr@ufm.edu>
2023-10-16 21:28:53 -0600

    Primeros cambios
    prueba de react con uso de un api de películas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// b0f0728c
+
+const API_URL = 'http://www.omdbapi.com?apikey=b0f0728c';
+const App = () => {
+    const [movies, SetMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        SetMovies(data.Search);
+    };
+
+    useEffect(() => {
+        searchMovies('Any');
+    }, []);
+
+    if (movies?.length > 0) {
+        return (
+            <div className={'app'}>
+                <h1>PopcornTime</h1>
+
+                <div className={'search'}>
+                    <input
+                        placeholder={'Look out for movies...'}
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <img
+                        src={SearchIcon}
+                        alt='search'
+                        onClick={() => searchMovies(searchTerm)}
+                    />
+                </div>
+                {
+                    <div className={'container'}>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                }
+            </div>
+        );
+    } else {
+        return (
+            <div className={'app'}>
+                <h1>PopcornTime</h1>
+
+                <div className={'search'}>
+                    <input
+                        placeholder={'Look out for movies...'}
+                        value={'Any'}
+                        onChange={() => {
+                        }}
+                    />
+                    <img
+                        src={SearchIcon}
+                        alt='search'
+                        onClick={() => {
+                        }}
+                    />
+                </div>
+
+                {
+                    <div className={'empty'}>
+                        <h2>No Movies Found</h2>
+                    </div>
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
