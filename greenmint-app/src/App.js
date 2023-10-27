3427f556cd8c363fb43c6af613b815c840b0273f
Milton Beltrán <miltonbeltran@ufm.edu>
2023-10-27 09:49:47 -0600

    pt2

---
+++ greenmint-app/src/App.js
@@ -0,0 +1,27 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Menu from './components/Menu';  // Asegúrate de tener este import
+import Search from './components/Search';
+import Meal from './components/Meal';
+import './App.css';
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <Header />
+                    <Routes>
+                        <Route path="/" element={<></>}/>
+                        <Route path="/menu" element={<Menu />} />
+                        <Route path="/search" element={<Search />} />
+                        <Route path="/meal" element={<Meal />} />
+
+                    </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
